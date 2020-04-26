53d28d7cdf27371149ef4a92f0fc954c71a43308
Artem <artemrandom@ya.ru>
2020-04-27 01:27:31 +0300

    a little alignments

--- src/MyLayout.js
+++ src/MyLayout.js
@@ -11,12 +11,11 @@ function MyLayout() {
       <main className="main">
         <Contacts/>
         <div className="focus-time">
-          <div className="msg">Focusing and time <span className="focus">are very important in our life</span></div>
+          <div className="msg">Focusing and time <span className="focus">are very important for our life.</span></div>
           <div className="msg care">I want to care about these human resources in products witch I creating.</div>
           <div className="msg write-me">
-            
             <span>Write me to <a href="https://t.me/artemshar" rel="me" target="_blanc" title="t.me/artemshar">telegram</a> </span>
-            <span>if you are looking Frontend / Product developer, like me: <a target="_blanc" href="https://docs.google.com/document/d/1Jtz-lmaFES7IfLbaUEjr84oWEab9kuHve5m7a8o49-0/edit?usp=sharing">CV</a></span>
+            <span>if you are looking Frontend / Product developer like me: <a target="_blanc" href="https://docs.google.com/document/d/1Jtz-lmaFES7IfLbaUEjr84oWEab9kuHve5m7a8o49-0/edit?usp=sharing">CV</a></span>
           </div>
           <div className="msg amazing">You're amazing! <br/> Dont lose your time {new Date().toLocaleTimeString()}</div>
         </div>
