79fa720b5d411c29dea122dc1937f4b6b9095264
Artem <artemrandom@ya.ru>
2020-05-06 02:47:29 +0300

    .

--- src/MyLayout.js
+++ src/MyLayout.js
@@ -9,11 +9,17 @@ function MyLayout() {
       <Header/>
       <main className="main">
         <div className="focus-time">
-          <div className="msg"><span className="focus">
-            I would like people to spend as little time as possible on fighting with interfaces
-            <br/><br/>
-            Everything should be easy to use
-            </span></div>
+          <div className="msg">
+            <div className="focus focus-1">
+              I would like people don't fight with interfaces and systems
+            </div>
+            <div className="focus focus-2">
+              Everything should be easy to use
+            </div>
+            <div className="focus focus-3">
+              Our time, attention, and mentality are very expensive things...
+            </div>
+          </div>
         </div>
       </main>
     </div>
