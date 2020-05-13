21d6a337526b965ec11663941026e194c43df2f9
Artem <artemrandom@ya.ru>
2020-05-13 23:17:33 +0300

    small text changes

--- src/MyLayout.js
+++ src/MyLayout.js
@@ -20,7 +20,13 @@ function MyLayout() {
               Our time, attention and emotions are very expensive things
             </div>
             <div className="focus focus-4">
-              And I would like to save more of these resources for real life, communication and something really interesting
+              As <a href="https://www.linkedin.com/in/artemshar/" target="_blanc">Frontend-developer</a>, I spend these resources so that users save them for real life, communication, and something really interesting
+            </div>
+            <br/>
+            <div className="focus focus-5">
+              I also think about how to find a balance between "automate everything" and "give people valuable work". Charity? Pay for brand loyalty and some activity?
+              I want to find an answer. I want to find an answer. You can say, that sociologists or governments should answer this question, but I think that in 2020 this is an important question for the founders of companies and managers.
+              <a href="https://twitter.com/artem_shar" target="_blanc">Tweet me</a> to start a dialog about this topic.
             </div>
           </div>
         </div>
