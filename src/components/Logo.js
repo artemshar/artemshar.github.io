d78edf17e3b3dcff133e824d4f3585c4b52d4e39
Artem <artemrandom@ya.ru>
2020-06-17 01:53:59 +0300

    logo-droch

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import cn from 'classnames'
 import './_logo.scss'
 
 const Logo = () => {
+  const [ lightClass, setLightClass ] = useState('')
+  const [ count, setCount ] = useState(1)
+
+  const handleClick = () => {
+    if (count <= 4 ) {
+      setCount(count + 1)
+    } else {
+      setCount(1)
+    }
+
+    setLightClass(`more-light-${count}`)
+  }
+  
   return (
-    <div className="logo-wrap">
+    <div className={cn("logo-wrap", lightClass)} onClick={() => handleClick()}>
       <svg className="logo-svg" viewBox="0 0 200 200" width="50px" height="50px">
         <circle className="logo-circle-1" cx="100" cy="100" r="100" fill="#e8e8e8" />
         <circle className="logo-circle-2" cx="100" cy="100" r="75" fill="#c5c5c5" />
